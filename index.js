3374db1c0040999c2607de115cc6f6e0c5774ef4
ball <nattawatx86@gmail.com>
2020-01-12 15:26:02 +0700

    first

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express")
+
+const http = require("http")
+const app = express()
+
+app.get("/jan", async (req, res) => {
+	let body = ""
+	let data = await new Promise(resolve => {
+		http.get(
+			"http://35.194.179.37:5000/api/getdata?fbclid=IwAR3cc15c4iNPP59v4w2IXQhXplJ6Yp39rkluGzVlgKM5RjoXvIy_yAQbu8g",
+			res => {
+				res.setEncoding("utf8")
+
+				res.on("data", data => {
+					body += data
+				})
+				res.on("end", () => {
+					body = JSON.parse(body)
+					resolve(body)
+				})
+			}
+		)
+	})
+	let arr = data.map(temp => {
+		return { time: temp["Timestamp"] }
+	})
+	res.send(arr)
+})
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT)
